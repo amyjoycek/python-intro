53069c3f60e810c08e9f2f35b05fc2cded38537b
Amy K <akostash4@gmail.com>
2019-12-04 16:58:29 -0500

    get repo setup

---
+++ Week1/intro.py
@@ -0,0 +1,20 @@
+print("Hello World!")
+print('Hello World!')
+a=1
+b=2
+x=a
+a=3
+b=x
+x=a
+print(a,b,x)
+a="nice"
+b="important"
+x=a
+y=b
+print(y, "to be", x)
+a="nice"
+b="important"
+temp_a=a
+a=b
+b=temp_a
+prlint(a, "to be", b)
